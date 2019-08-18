57f45cea80c9df4d195833cb753042a2d4693a2a
William Weisberger <bill.weisberger@gmail.com>
2019-08-18 11:30:09 -0600

    initial commit from local to remote github repo

--- src/components/about me/index.js
+++ src/components/about me/index.js
@@ -2,24 +2,24 @@ import React from 'react'
 import {Grid, Cell} from 'react-mdl'
 
 const About = () =>{
-        return(
-            <Grid className="aboutme-grid">
-                <Cell className='left-col' col={8}>
-                    <h2 className="aboutme-intro">Hi, I'm Bill</h2>
-                    <div className='aboutme-img-container'>
-                        <img className='aboutme-img'src="images/DSC03328.jpg" alt='Bill Weisberger'/>
-                    </div>
-                    <h4>A little bit about me:</h4>
-                    <p>I love helping people, which is why I worked as a high school science teacher for 6 years.</p>
-                    <p>I love to learn new skills and build useful things, which is why I became a full stack dev.</p>
-                    <p>I love my daughter Nora, because she's cute.</p>
-                    <p>I have been training as a full stack developer for three months - 40 hours a week in class and </p>
-                </Cell>
-                <Cell className='right-col' col={4}>
-                    
-                </Cell>
-            </Grid> 
-        )
-    }
+    return(
+        <Grid className="aboutme-grid">
+            <Cell className='left-col' col={8}>
+                <h2 className="aboutme-intro">Hi, I'm Bill</h2>
+                <div className='aboutme-img-container'>
+                    <img className='aboutme-img'src="images/DSC03328.jpg" alt='Bill Weisberger'/>
+                </div>
+                <h4>A little bit about me:</h4>
+                <p>I love helping people, which is why I worked as a high school science teacher for 6 years.</p>
+                <p>I love to learn new skills and build useful things, which is why I became a full stack dev.</p>
+                <p>I love my daughter Nora, because she's cute.</p>
+                <p>I have been training as a full stack developer for three months - 40 hours a week in class and </p>
+            </Cell>
+            <Cell className='right-col' col={4}>
+                
+            </Cell>
+        </Grid> 
+    )
+}
 
 export default About;
--- src/components/landingpage/index.js
+++ src/components/landingpage/index.js
@@ -5,7 +5,7 @@ import TextLoop from 'react-text-loop';
 class Landing extends Component{
     render(){
         return(
-            <div style={{width: '100%', margin: 'auto'}}>
+            <main className="home-div" style={{width: '100%', margin: 'auto'}}>
                 <Grid className='landing-grid'>
                     <Cell col={7}>
                         <div className="hero-text">
@@ -49,7 +49,24 @@ class Landing extends Component{
                         </div>
                     </Cell>
                 </Grid>
-            </div>
+
+                <Grid className="aboutme-grid">
+                    <Cell className='left-col' col={8}>
+                        <h2 className="aboutme-intro">Hi, I'm Bill</h2>
+                        <div className='aboutme-img-container'>
+                            <img className='aboutme-img'src="images/DSC03328.jpg" alt='Bill Weisberger'/>
+                        </div>
+                        <h4>A little bit about me:</h4>
+                        <p>I love helping people, which is why I worked as a high school science teacher for 6 years.</p>
+                        <p>I love to learn new skills and build useful things, which is why I became a full stack dev.</p>
+                        <p>I love my daughter Nora, because she's cute.</p>
+                        <p>I have been training as a full stack developer for three months - 40 hours a week in class and </p>
+                    </Cell>
+                    <Cell className='right-col' col={4}>
+                        
+                    </Cell>
+                </Grid> 
+            </main>
         )
     }
 }
