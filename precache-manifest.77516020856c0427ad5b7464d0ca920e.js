3af213a7cd2f4a76907c42103adfebbf12476b12
William Weisberger <bill.weisberger@gmail.com>
2019-08-18 12:29:25 -0600

    Updates

---
+++ precache-manifest.77516020856c0427ad5b7464d0ca920e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491a98db06ad7db5db54484b30b4cf88",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3a921676e0521f3eb42c",
+    "url": "/react-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "20ed329fcea0eb556990",
+    "url": "/react-portfolio/static/css/main.49278b9a.chunk.css"
+  },
+  {
+    "revision": "3a921676e0521f3eb42c",
+    "url": "/react-portfolio/static/js/2.8bd80168.chunk.js"
+  },
+  {
+    "revision": "20ed329fcea0eb556990",
+    "url": "/react-portfolio/static/js/main.87ec172a.chunk.js"
+  },
+  {
+    "revision": "4d1d151b896bea087de4",
+    "url": "/react-portfolio/static/js/runtime~main.3f94e198.js"
+  }
+]);
