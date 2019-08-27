9d96fac907dde8cb976365bc20febec79868fd15
William Weisberger <bill.weisberger@gmail.com>
2019-08-26 20:50:01 -0600

    added project images, urls, hrefs to project cards

--- src/components/project_cards/index.js
+++ src/components/project_cards/index.js
@@ -4,21 +4,23 @@ import {Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton} fr
 const ProjectCard = (props) => {
 
     return(
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: 'black', height: '176px', background:`url(${props.url}) center/cover`}}>
-                {props.title}
-            </CardTitle>
-            <CardText>
-                {props.text}
-            </CardText>
-            <CardActions border>
-                <Button colored>Github</Button>
-                <Button colored>LiveDemo</Button>
-            </CardActions>
-            <CardMenu style={{color: 'black'}}>
-                <IconButton name="share"/>
-            </CardMenu>
-        </Card>
+        
+            <Card href={props.href1} shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <a href={props.href1}><CardTitle style={{color: 'white', height: '176px', background:`url(${props.url}) center/cover`}}>
+                    {props.title}
+                </CardTitle></a>
+                <CardText>
+                    {props.text}
+                </CardText>
+                <CardActions border>
+                    <Button target="_blank" href={props.href2} colored>Github</Button>
+                    {/* <Button href="" colored>LiveDemo</Button> */}
+                </CardActions>
+                <CardMenu style={{color: 'black'}}>
+                    <IconButton name="share"/>
+                </CardMenu>
+            </Card>
+        
     )
 }
 
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -14,49 +14,21 @@ class Projects extends Component{
                 <div className="projects-grid">
                     {/* Project 1 */}
                     <ProjectCard 
-                        title="React Project 1" 
-                        url="#" 
-                        text="This project uses react"
+                        title="React Portfolio" 
+                        href1='www.billweisberger.com/react-portfolio'
+                        href2="http://www.github.com/bweisberger/react-portfolio" 
+                        url="/images/ReactPortfolio.png" 
+                        text="Current Page"
+                        href="www.billweisberger.com"
                     />
 
-                    {/* Project 2 */}
                     <ProjectCard 
-                        title="React Project 2" 
-                        url="#" 
-                        text="This project uses react"
+                        title="FilmFetch" 
+                        url="/images/reactjs.png" 
+                        text="Coming Soon: API Calls, Python with Flask, and React"
                     />
 
-                    {/* Project 3 */}
-                    <ProjectCard 
-                        title="React Project 3" 
-                        url="#" 
-                        text="This project uses react"
-                    />
-                </div>
-            )
-        } else if(this.state.activeTab === 1){
-            return(
-                <div className="projects-grid">
-                    {/* Project 1 */}
-                    <ProjectCard 
-                        title="Node Project 1" 
-                        url="#" 
-                        text="This project uses NodeJS"
-                    />
-
-                    {/* Project 2 */}
-                    <ProjectCard 
-                        title="Node Project 2" 
-                        url="#" 
-                        text="This project uses NodeJS"
-                    />
-
-                    {/* Project 3 */}
-                    <ProjectCard 
-                        title="Node Project 3" 
-                        url="#" 
-                        text="This project uses NodeJS"
-                    />
+                    
                 </div>
             )
         } else if(this.state.activeTab === 2){
@@ -64,23 +36,34 @@ class Projects extends Component{
                 <div className="projects-grid">
                     {/* Project 1 */}
                     <ProjectCard 
-                        title="jQuery Project 1" 
-                        url="#" 
-                        text="This project uses jQuery"
+                        title="Down To Play"
+                        href1='http://down-to-play.herokuapp.com'
+                        href2='https://github.com/ssmich/DTP'
+                        url="/images/DTP_Image.png" 
+                        text="This project uses NodeJS, Express, and Bootstrap"
+                    />
+
+                </div>
+            )
+        } else if(this.state.activeTab === 1){
+            return(
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <ProjectCard 
+                        title="Homer's Day Off" 
+                        href1='https://billweisberger.com/Simpsons-Tamagotchi'
+                        href2="https://github.com/bweisberger/Simpsons-Tamagotchi"
+                        url="/images/Simpsons_Game.png" 
+                        text="This project uses jQuery, Javascript, HTML5 and CSS3"
                     />
 
                     {/* Project 2 */}
                     <ProjectCard 
-                        title="jQuery Project 2" 
-                        url="#" 
-                        text="This project uses jQuery"
-                    />
-
-                    {/* Project 3 */}
-                    <ProjectCard 
-                        title="jQuery Project 3" 
-                        url="#" 
-                        text="This project uses jQuery"
+                        title="Minimalist Megaman" 
+                        href1="https://billweisberger.com/Game-Project"
+                        href2='https://github.com/bweisberger/Game-Project'
+                        url="/images/MinimalistMegaman.png" 
+                        text="This project uses jQuery, Javascript, HTML5 and CSS3"
                     />
                 </div>
             )
@@ -89,23 +72,27 @@ class Projects extends Component{
                 <div className="projects-grid">
                     {/* Project 1 */}
                     <ProjectCard 
-                        title="API Project 1" 
-                        url="#" 
-                        text="This project uses APIs"
+                        title="Giphy API Project" 
+                        href1='https://billweisberger.com/giphy-react-hw'
+                        href2='https://github.com/bweisberger/giphy-react-hw'
+                        url="/images/reactjs.png" 
+                        text="This project uses APIs to search for gifs based on your search"
                     />
 
                     {/* Project 2 */}
                     <ProjectCard 
-                        title="API Project 2" 
-                        url="#" 
-                        text="This project uses APIs"
+                        title="FilmFetch" 
+                        url="/images/reactjs.png" 
+                        text="Coming Soon: API Calls, Python with Flask, and React"
                     />
 
                     {/* Project 3 */}
                     <ProjectCard 
-                        title="API Project 3" 
-                        url="#" 
-                        text="This project uses APIs"
+                        title="News API"
+                        href1="https://billweisberger.com/sandwich-news" 
+                        href2='https://github.com/bweisberger/sandwich-news'
+                        url="/images/reactjs.png" 
+                        text="This project accesses news about any searched topic using a 3rd party API"
                     />
                 </div>
             )
